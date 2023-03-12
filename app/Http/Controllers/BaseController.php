f7b5f003c916a66ab654573cdd10ee5a61e90804
Lai Tuan Vu <tuanvu101289@gmail.com>
2023-03-12 12:39:35 +0700

    update directory

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -12,11 +12,13 @@ class BaseController extends Controller
     public $layoutDir;
     public function __construct()
     {
-
+        // get domain
         $this ->domain = parse_url(request()->root())['host'];
+        // get website
         $websiteService  = new WebsiteService();
         $this -> website = $websiteService -> findByDomain($this ->domain);
-        $this -> layoutDir = 'client.guesthouse';
+        // get directory 
+        $this -> layoutDir = $this->website->layout['directory'];
         view()->share('viewDir', $this -> layoutDir );
         view()->share('website', $this -> website );
     }
