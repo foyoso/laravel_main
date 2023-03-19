ca49e49790d6495fb4b6499414ff4c3c0d7b59b7
Lai Tuan Vu <tuanvu101289@gmail.com>
2023-03-19 14:44:44 +0700

    change layout findhouse

---
+++ resources/views/client/findhouse/common/js.blade.php
@@ -0,0 +1,21 @@
+<!-- Wrapper End -->
+<script type="text/javascript" src="/client/findhouse/js/jquery-3.3.1.js"></script>
+<script type="text/javascript" src="/client/findhouse/js/jquery-migrate-3.0.0.min.js"></script>
+<script type="text/javascript" src="/client/findhouse/js/popper.min.js"></script>
+<script type="text/javascript" src="/client/findhouse/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="/client/findhouse/js/jquery.mmenu.all.js"></script>
+<script type="text/javascript" src="/client/findhouse/js/ace-responsive-menu.js"></script>
+<script type="text/javascript" src="/client/findhouse/js/bootstrap-select.min.js"></script>
+<script type="text/javascript" src="/client/findhouse/js/isotop.js"></script>
+<script type="text/javascript" src="/client/findhouse/js/snackbar.min.js"></script>
+<script type="text/javascript" src="/client/findhouse/js/simplebar.js"></script>
+<script type="text/javascript" src="/client/findhouse/js/parallax.js"></script>
+<script type="text/javascript" src="/client/findhouse/js/scrollto.js"></script>
+<script type="text/javascript" src="/client/findhouse/js/jquery-scrolltofixed-min.js"></script>
+<script type="text/javascript" src="/client/findhouse/js/jquery.counterup.js"></script>
+<script type="text/javascript" src="/client/findhouse/js/wow.min.js"></script>
+<script type="text/javascript" src="/client/findhouse/js/slider.js"></script>
+<script type="text/javascript" src="/client/findhouse/js/pricing-slider.js"></script>
+<script type="text/javascript" src="/client/findhouse/js/timepicker.js"></script>
+<!-- Custom script for all pages -->
+<script type="text/javascript" src="/client/findhouse/js/script.js"></script>
