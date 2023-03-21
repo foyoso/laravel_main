eb16c508e913215b9f7c8b27e46cb12139f681a2
Lai Tuan Vu <tuanvu101289@gmail.com>
2023-03-21 23:52:25 +0700

    update layout

--- routes/web/clientRoutes.php
+++ routes/web/clientRoutes.php
@@ -2,7 +2,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
 
-use App\Http\Controllers\BookingController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\DetailController;
 use App\Http\Controllers\BlogsController;
@@ -14,4 +13,3 @@ Route::get('/contact',[ContactController::class, 'index']);
 Route::get('/detail',[DetailController::class, 'index']);
 Route::get('/listing',[ListingController::class, 'index']);
 Route::get('/blog',[BlogsController::class, 'index']);
-Route::get('/booking',[BookingController::class, 'index']);
